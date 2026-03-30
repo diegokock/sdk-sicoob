8afd3e8b1a1e6dea7d85d084054ffa0d6c276fc1
dev <diegokock90@gmail.com>
2026-03-30 16:50:26 -0300

    feat: contexto de conta corrente criado

---
+++ src/Contexts/ContaCorrente.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Diegokock\SdkSicoob\Contexts;
+
+use GuzzleHttp\Exception\GuzzleException;
+use Diegokock\SdkSicoob\Configuration;
+use Diegokock\SdkSicoob\Helpers\CallApi;
+
+class ContaCorrente extends CallApi
+{
+    public function __construct(Configuration $config)
+    {
+        parent::__construct($config);
+    }
+
+    /**
+     * Consulta o extrato de uma conta corrente.
+     *
+     * @param int      $mes          Mês de referência (1-12)
+     * @param int      $ano          Ano de referência ex: 2026
+     * @param int      $numeroConta  Número da conta corrente
+     * @param int|null $diaInicial   Dia inicial para filtrar (opcional)
+     * @param int|null $diaFinal     Dia final para filtrar (opcional)
+     * @param bool     $agrupaCNAB   Agrupar movimentos provenientes do CNAB
+     *
+     * @throws GuzzleException
+     */
+    public function extrato(
+        int  $mes,
+        int  $ano,
+        int  $numeroConta,
+        ?int $diaInicial  = null,
+        ?int $diaFinal    = null,
+        bool $agrupaCNAB  = false
+    ): object {
+        $query = array_filter([
+            'numeroConta' => $numeroConta,
+            'diaInicial'  => $diaInicial,
+            'diaFinal'    => $diaFinal,
+            'agrupaCNAB'  => $agrupaCNAB ? 'true' : null,
+        ], fn($v) => !is_null($v));
+
+        return $this->call(
+            "/conta-corrente/v4/extrato/{$mes}/{$ano}",
+            $query
+        );
+    }
+
+    /**
+     * Consulta o saldo disponível da conta corrente.
+     *
+     * @param int $numeroConta Número da conta corrente
+     *
+     * @throws GuzzleException
+     */
+    public function saldo(int $numeroConta): object
+    {
+        return $this->call(
+            '/conta-corrente/v4/saldo',
+            ['numeroConta' => $numeroConta]
+        );
+    }
+}
